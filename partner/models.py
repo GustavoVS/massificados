966421b940fdf67fb4e09553a87d99c7aaf92773
Gustavo Lima da Silva <gustavo.silva.vs@gmail.com>
2016-04-17 12:34:28 -0300

    -Models de Product, Partner e Sac

--- partner/models.py
+++ partner/models.py
@@ -2,16 +2,18 @@
 from __future__ import unicode_literals
 from django.db import models
 from product.models import Product
+from django.utils import timezone
+from django.utils.translation import ugettext_lazy as _
 
 
 class Partner(models.Model):
     name = models.CharField(max_length=100)
-    galcorr_logo = models.CharField(max_length=80)
-    parner_logo = models.CharField(max_length=80)
-    url = models.CharField(max_length=250)
-    create = models.DateField()
+    logo = models.FilePathField()
+    slug = models.CharField(max_length=50)
+    date_create = models.DateTimeField(_('Date created'), default=timezone.now)
     modificate = models.DateField()
     email = models.EmailField()
+    cnpj = models.CharField(max_length=18)
 
 
 class Campaign(models.Model):
@@ -25,5 +27,40 @@ class Campaign(models.Model):
         on_delete=models.CASCADE,
     )
 
+
+class Sac(models.Model):
+    name = models.CharField(max_length=100)
+    partner = models.ForeignKey(
+        Partner,
+        on_delete=models.CASCADE
+    )
+
+
+class SacItem(models.Model):
+    name = models.CharField(max_length=100)
+    detail = models.TextField()
+    sac = models.ForeignKey(
+        Sac,
+        on_delete=models.CASCADE
+    )
+
+
+class SacPhone(models.Model):
+    description = models.CharField(max_length=100)
+    number = models.CharField(max_length=30)
+    sac_item = models.ForeignKey(
+        SacItem,
+        on_delete=models.CASCADE
+    )
+
+class Adress():
+    pass
+
+
+class SocialMedia():
+    pass
+
+
 class Template():
     pass
+
